c42fc61876443d84a48384fd16bf9d3124076a85
Muneeswara12 <kagithammunik@gmail.com>
2023-07-08 21:57:42 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.db.models.functions import Length
+from app.models import *
+# Create your views here.
+
+def display_topics(request):
+    LTO=Topic.objects.all()
+    #LTO=Topic.objects.filter(topic_name='Cricket')
+    #LTO=Topic.objects.get(topic_name='Cricket')
+    
+    d={'LTO':LTO}
+    return render(request, 'display_topics.html',d)
+
+def display_webpages(request):
+    LWO=Webpage.objects.all()
+    LWO=Webpage.objects.filter(topic_name='Cricket')
+    LWO=Webpage.objects.filter(topic_name='Volley Ball')
+    LWO=Webpage.objects.exclude(topic_name='Volley Ball')
+    LWO=Webpage.objects.all()[3::]
+    LWO=Webpage.objects.all()[::-1]
+    LWO=Webpage.objects.all().order_by('name')
+    LWO=Webpage.objects.all().order_by('-name')
+    LWO=Webpage.objects.all().order_by(Length('name'))
+    LWO=Webpage.objects.all().order_by(Length('name').desc())
+    LWO=Webpage.objects.all()
+    d={'LWO':LWO}
+    return render(request,'display_webpages.html',d)
+
+def display_access(request):
+    access=AccessRecords.objects.all()
+
+    d={'access':access}
+    return render(request,'display_access.html',d)
